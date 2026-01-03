35a19e3657175e44069fbb1ab488b5b125ac8474
Pankaj Kumar <liamlive19@gmail.com>
2026-01-03 22:07:45 +0530

    changes number to dices

--- Components/Dice.jsx
+++ Components/Dice.jsx
@@ -1,12 +1,30 @@
 import "./main.css"
 
+
 function Dice(props){
+    
+    
+       // console.log(currentImage)    
+        
+    
+    function selectDice(){
+       props.hold(props.id)
+       
+       
+    }
+    
+
 const styles ={
-    backgroundColor : props.isHeld ? "#59E391" : "white"
+    //backgroundColor : props.isHeld ? "#59E391" : "white"
+    backgroundImage: `url(${props.url})`,
+    backgroundSize: "cover",
+    backgroundRepeat: "no-repeat",
+    backgroundPosition: "center"
+
 }
     return  <button className="dice" 
                     style={styles}
-                    onClick={()=>{props.hold(props.id)}}
-                    >{props.value}</button>
+                    onClick={selectDice}
+                    ></button>
 }
 export default Dice
--- Components/Main.jsx
+++ Components/Main.jsx
@@ -1,11 +1,29 @@
 import "./main.css"
 import Dice from "./Dice";
+import Dice1 from "/dice_1.png"
+import Dice2 from "/dice_2.png"
+import Dice3 from "/dice_3.png"
+import Dice4 from "/dice_4.png"
+import Dice5 from "/dice_5.png"
+import Dice6 from "/dice_6.png"
+import Dark1 from "/dark_dice_1.png"
+import Dark2 from "/dark_dice_2.png"
+import Dark3 from "/dark_dice_3.png"
+import Dark4 from "/dark_dice_4.png"
+import Dark5 from "/dark_dice_5.png"
+import Dark6 from "/dark_dice_6.png"
 import { useEffect, useState ,useRef} from "react";
 import Confetti from 'react-confetti'
 
  function Main(){
     const message = "Roll until all dice are the same. Click each die to freeze it at its current value between rolls.";
+    const imagesArray = [Dice1,Dice2,Dice3,Dice4,Dice5,Dice6]
+    const darkImages = [Dark1,Dark2,Dark3,Dark4,Dark5,Dark6]
     const [dice,setDice] = useState(()=>generateArray())
+
+  
+  //console.log(dice)
+   // const [currentImage , setCurrentImage]= useState("")
    // const [roll,setRoll] = useState("Roll")
     //const { width, height } = useWindowSize()
 
@@ -13,9 +31,10 @@ import Confetti from 'react-confetti'
         let array = [];
         
         for(let i=0;i<10;i++){
-            const rand = Math.ceil(Math.random()*3)
+            const rand = Math.floor(Math.random()*6)
             const obj = {
-                value : rand,
+                num: rand,
+                url : imagesArray[rand],
                 isHeld :false,
                 id:i
             }
@@ -27,7 +46,7 @@ import Confetti from 'react-confetti'
                                        ( <Dice
                                              key={numObj.id} 
                                              isHeld={numObj.isHeld}
-                                             value={numObj.value}
+                                             url={numObj.url}
                                              id={numObj.id}
                                              hold={hold}
                                              />))
@@ -35,13 +54,22 @@ import Confetti from 'react-confetti'
    function rollDice() {
      won ?
       setDice(generateArray()) : 
-       setDice(prevDice =>
-        prevDice.map(die =>
-            die.isHeld
-                ? die
-                : { ...die, value: Math.ceil(Math.random() * 6) }
-        )
-    );
+       setDice(oneDie=>{
+       return oneDie.map(die=>{
+        if(die.isHeld){
+            return die;
+        }else{
+            const random = Math.floor(Math.random()*6);
+           return {
+                ...die,
+                num : random,
+                url : imagesArray[random]
+            }
+
+        }
+        })
+
+       })
 }
 
 
@@ -49,7 +77,9 @@ import Confetti from 'react-confetti'
     setDice(prevDice =>
         prevDice.map(oneDice =>
             oneDice.id === id
-                ? { ...oneDice, isHeld: !oneDice.isHeld }
+                ? { ...oneDice,
+                     url : oneDice.isHeld ? imagesArray[oneDice.num] : darkImages[oneDice.num],
+                     isHeld: !oneDice.isHeld }
                 : oneDice
         )
     );
@@ -61,7 +91,7 @@ const buttonRef = useRef(null)
 
         const won = 
         dice.every(die => die.isHeld) &&
-        dice.every(die => die.value === dice[0].value)
+        dice.every(die => die.url === dice[0].url)
  
 useEffect(()=>{
     if(won){
@@ -69,7 +99,7 @@ useEffect(()=>{
     }
 },[won])
 
-// console.log(dice)
+ //console.log(dice)
 
     return (
         <main>
@@ -80,7 +110,7 @@ useEffect(()=>{
             </div>
             <button ref={buttonRef} className="roll-btn" onClick={rollDice}>{won ? "New Game" : "Roll"}</button>
             {won && <Confetti
-              width={window.width}
+              width={window.innerWidth}
             /> }
             {/* <Confetti/> */}
         </main>
